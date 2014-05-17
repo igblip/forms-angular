f91ec417a9a5ccc0bdc42230839bf330eae986fa
gstevens <gstevens@guidelightsolutions.com>
2014-05-17 10:48:26 -0500

    demo files should be in the demo

--- app/demo/demo.js
+++ app/demo/demo.js
@@ -5,19 +5,19 @@ var myDemoApp = angular.module('myDemoApp', ['formsAngular']);
 myDemoApp.run(['tele', function (tele) {
 
     tele.routes( 'demo', 'demo/', [
-        { path: '/index',                           route: {templateUrl: 'partials/landing-page.html'} },
-        { path: '/get-started',                     route: {templateUrl: 'partials/get-started.html' } },
-        { path: '/forms',                           route: {templateUrl: 'partials/forms.html'       } },
-        { path: '/schemas',                         route: {templateUrl: 'partials/schemas.html'     } },
-        { path: '/reporting',                       route: {templateUrl: 'partials/reporting.html'   } },
-        { path: '/more',                            route: {templateUrl: 'partials/more.html'        } },
-        { path: '/in-the-wild',                     route: {templateUrl: 'partials/in-the-wild.html' } },
-        { path: '/examples',                        route: {templateUrl: 'partials/examples.html'    } },
-        { path: '/api-docs',                        route: {templateUrl: 'partials/api-docs.html'    } },
-        { path: '/z_custom_form/new',               route: {templateUrl: 'partials/custom-new.html'  } },
-        { path: '/z_custom_form/:id/edit',          route: {templateUrl: 'partials/custom-edit.html' } },
-        { path: '/z_custom_form/:form/new',         route: {templateUrl: 'partials/custom-new.html'  } },
-        { path: '/z_custom_form/:form/:id/edit',    route: {templateUrl: 'partials/custom-edit.html' } }
+        { path: '/index',                           route: {templateUrl: 'demo/partials/landing-page.html'} },
+        { path: '/get-started',                     route: {templateUrl: 'demo/partials/get-started.html' } },
+        { path: '/forms',                           route: {templateUrl: 'demo/partials/forms.html'       } },
+        { path: '/schemas',                         route: {templateUrl: 'demo/partials/schemas.html'     } },
+        { path: '/reporting',                       route: {templateUrl: 'demo/partials/reporting.html'   } },
+        //{ path: '/more',                            route: {templateUrl: 'partials/more.html'        } },
+        { path: '/in-the-wild',                     route: {templateUrl: 'demo/partials/in-the-wild.html' } },
+        { path: '/examples',                        route: {templateUrl: 'demo/partials/examples.html'    } },
+        { path: '/api-docs',                        route: {templateUrl: 'demo/partials/api-docs.html'    } },
+        { path: '/z_custom_form/new',               route: {templateUrl: 'demo/partials/custom-new.html'  } },
+        { path: '/z_custom_form/:id/edit',          route: {templateUrl: 'demo/partials/custom-edit.html' } },
+        { path: '/z_custom_form/:form/new',         route: {templateUrl: 'demo/partials/custom-new.html'  } },
+        { path: '/z_custom_form/:form/:id/edit',    route: {templateUrl: 'demo/partials/custom-edit.html' } }
     ]);
 
     tele.defaultRoute('/demo/index');
